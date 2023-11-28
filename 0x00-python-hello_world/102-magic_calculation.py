60bb1a75e565427d2c32b1aea73a8f8bded9d046
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-11-27 18:30:04 -0800

    not a magic at at all

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-return 98 + a ** b
+    from magic_calculation import add, sub
+    if a < b:
+        y = add(a, b)
+        for z in range(4, 6):
+            y = add(c, z)
+            return y
+        else:
+            return sub(a, b)b

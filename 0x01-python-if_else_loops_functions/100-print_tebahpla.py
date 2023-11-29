e2a6c96db0a725970103f5ea8d19dc546d61cdcc
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-11-28 18:17:34 -0800

    if else loops funcs

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for x in range(122, 96, -1):
-    if x % 2:
-        x = x - 32
-        print("{:c}".format(x), end="")
+for x in range(ord('z'), ord('a') -1, -2):
+    print("{}{}".format(chr(x), chr(x - 33)), end='')

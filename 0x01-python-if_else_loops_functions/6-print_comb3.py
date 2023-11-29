11b82d82c7c6f03bb43a6e89ccafebb434c2fea5
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-11-28 17:16:01 -0800

    if else loops funcs

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for x in range(9):
     for y in range(x + 1, 10):
         if x * 10 + y < 89:
-            print("{:d}{:d}".format(x, y), end= ", ")
+            print("{:d}{:d}".format(x, y), end=", ")
 print("{:d}".format(89))

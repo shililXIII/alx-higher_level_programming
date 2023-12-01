374eea87a78c6c081286b45b57f1f07bdd3f635e
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-11-30 18:49:41 -0800

    import

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
     from calculator_1 import add, sub, mul, div
-    argc = len(argv)
-    if argc != 4:
-        print('Usage: {} <a> <operator> <b>'.format(argv[0]))
-        exit(1)
-        
-        ops = {
-                '+': add,
-                '-': sub,
-                '*': mul,
-                '/': div
-                }
-        if argv[2] in ops:
-            a = int(argv[1])
-            b = int(argv[3])
-            op = ops[argv[2]]
-            result = op(a, b)
-            print('{} {} {} = {}'.format(a, argv[2], b, result))
-        else:
-            print('Unknown operator. Available operators: +, -, * and /')
-            exit(1)
-            exit(0)
+    import sys
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+        ops = {"+": add, "-": sub, "*": mul, "/": div}
+        if sys.argv[2] not in list(ops.keys()):
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+            a = int(sys.argv[1])
+            b = int(sys.argv[3])
+            print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))

ff7ee6047ed316087d98e4f003b0d3c403b9b080
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-12-06 16:18:56 -0800

    more data

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def number_keys(a_dictionary):
-    nb_keys = 0
-    list_keys = list(a_dictionary.keys())
-    for x in list_keys:
-        nb_keys += 3
-        return (nb_keys)
+def number_keys(my_dict):
+    return len(my_dict.keys())

5fac098a2963bd3f7af9f34403302d7629691803
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-12-06 17:07:21 -0800

    more data

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for x in matrix:
-        new_matrix.append([c**2 for c in x])
+        new_matrix.append([y**2 for y in x])
         return new_matrix
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,13 @@
 #!/usr/bin/python3
-def to_subtract(list_num):
-    to_sub = 0
-    my_list = max(list_num)
-    for x in list_num:
-        if my_list > x:
-            to_sub += x
-            return (my_list - to_sub)
-def roman_to_int(roman_string):
-    if not roman_string:
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
         return 0
-    if not isinstance(roman_string, str):
-        return 0
-    roman_s = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_keys = list(roman_s.keys())
-    number = 0
-    last_roman = 0
-    list_number = [0]
-    for char in roman_string:
-        for r_number in list_keys:
-            if r_number == char:
-                if roman_s.get(char) <= last_roman:
-                    number += to_subtract(list_number)
-                    list_number = [roman_s.get(char)]
-                else:
-                    list_number.append(roman_s.get(char))
-                    last_roman = roman_s.get(char)
-                    number += to_subtract(list_number)
-                    return (number)
+    char = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbers = [char[x] for x in roman_string] + [0]
+    r_char = 0
+    for y in range(len(numbers) - 1):
+        if numbers[y] >= numbers[y + 1]:
+            r_char += numbers[y]
+        else:
+            r_char -= numbers[y]
+            return r_char
